0012cfdd1631f851e2612f1414232ee810be448d
ende <endeshen99@gmail.com>
2023-01-18 20:52:48 -0800

    finished client side testing (might be bugs)

--- src/client.ts
+++ src/client.ts
@@ -6,11 +6,35 @@ const SERVER_HOST = '149.28.200.131';
 // const SERVER_HOST = '0.0.0.0';
 const SERVER_PORT = 18018;
 
-// test case 0: a mal formatted messgae
+
+// test case for varies mal-formed messages 
+function test_mal_messages(message: string): void {
+    const client = new net.Socket();
+    console.log("--------------------------------");
+    console.log("test case for varies mal-formed messages ");
+    client.connect(SERVER_PORT, SERVER_HOST, async () => {
+        console.log('Connected to server.');
+        client.write(Messages.helloMessage.json + '\n');
+        client.write(message + '\n');
+        await delay(1000);
+        client.destroy();
+    });
+
+    client.on('data', (data) => {
+        console.log(`Server sent: ${data}`);
+    })
+
+    client.on('close', () => {
+        console.log('Server disconnected');
+    })
+}
+
+
+// test case 0: a mal formatted messgae up front
 function test_0(): void {
     const client = new net.Socket();
     console.log("--------------------------------");
-    console.log("test case 0: a mal formated message to node.");
+    console.log("test case 0: a mal formatted messgae up front");
     client.connect(SERVER_PORT, SERVER_HOST, async () => {
         console.log('Connected to server.');
         client.write('TEST_0: this is some random message.\n');
@@ -69,14 +93,113 @@ function test_2(): void {
     })
 }
 
+// test case 3: getPeers() but over two packages
+function test_3(): void {
+    const client = new net.Socket();
+    console.log("--------------------------------");
+    console.log("test case 3: getPeers() but over two packages")
+    client.connect(SERVER_PORT, SERVER_HOST, async () => {
+        console.log('Connected to server.');
+        client.write(Messages.helloMessage.json + '\n');
+        client.write('{"type":ge');
+        await delay(100);
+        client.write('tpeers"}\n');
+        await delay(1000);
+        client.destroy();
+    });
+    client.on('data', (data) => {
+        console.log(`Server sent: ${data}`);
+    })
+    
+    client.on('close', () => {
+        console.log('Server disconnected');
+    })
+}
+
+// test case 4: getPeers() but didn't send hello first
+function test_4(): void {
+    const client = new net.Socket();
+    console.log("--------------------------------");
+    console.log("test case 4: getPeers() but didn't send hello first")
+    client.connect(SERVER_PORT, SERVER_HOST, async () => {
+        console.log('Connected to server.');
+        client.write(Messages.getPeersMessage.json + '\n');
+        await delay(1000);
+        client.destroy();
+    });
+    client.on('data', (data) => {
+        console.log(`Server sent: ${data}`);
+    })
+    
+    client.on('close', () => {
+        console.log('Server disconnected');
+    })
+}
+
+// test case 5: getpeers() after send peers 
+async function test_5(): Promise<void> {
+
+    const peers_msg = JSON.stringify({
+        "type": "peers",
+        "peers": [
+          "dionyziz.com:18018" /* dns */,
+          "138.197.191.170:18018" /* ipv4 */,
+          "[fe80::f03c:91ff:fe2c:5a79]:18018" /* ipv6 */
+        ]
+    });
+    
+    var client = new net.Socket();
+    console.log("--------------------------------");
+    console.log("test case 5 PART1: getpeers() after send peers ")
+    client.connect(SERVER_PORT, SERVER_HOST, async () => {
+        console.log('Connected to server.');
+        client.write(Messages.helloMessage.json + '\n');
+        client.write(peers_msg + '\n');
+        await delay(1000);
+        client.destroy();
+    });
+    client.on('data', (data) => {
+        console.log(`Server sent: ${data}`);
+    })
+    
+    client.on('close', () => {
+        console.log('Server disconnected');
+    })
+
+    // wait between two consecutive connections
+    await delay(5000);
+
+    client = new net.Socket();
+    console.log("--------------------------------");
+    console.log("test case 5 PART2: getpeers() after send peers ")
+    client.connect(SERVER_PORT, SERVER_HOST, async () => {
+        console.log('Connected to server.');
+        client.write(Messages.helloMessage.json + '\n');
+        client.write(Messages.getPeersMessage.json + '\n');
+        await delay(1000);
+        client.destroy();
+    });
+    client.on('data', (data) => {
+        console.log(`Server sent: ${data}`);
+    })
+    
+    client.on('close', () => {
+        console.log('Server disconnected');
+    })
+
+}
+
 var tests = [
-    test_0,
-    test_1,
-    test_2,
+    test_0,  // test case 0: a mal formatted messgae up front           FAIl
+    test_1,  // test case 1: hello and then mal formatted message       FAIl
+    test_2,  // test case 2: getPeers()                                 SUCCESS
+    test_3,  // test case 3: getPeers() but over two packages           SUCCESS
+    test_4,  // test case 4: getPeers() but didn't send hello first     FAIL
+    test_5,  // test case 5: getpeers() after send peers                SUCCESS
 ]
 
 async function test(): Promise<void> {
-    const total_tests = 3;
+    const total_tests = tests.length;
     for (let i = 0; i < total_tests; i++) {
         tests[i]();
         if (i != total_tests-1) {await delay(5000);}
@@ -84,3 +207,18 @@ async function test(): Promise<void> {
 };
 
 test();
+
+
+// testing all sorts of mal-formed messages                             FAIL
+const invalid_messages = [
+    'Wbgygvf7rgtyv7tfbgy{{{',
+    '{"type":"diufygeuybhv"}',
+    '{"type":"hello"}',
+    '{"type":"hello","version":"jd3.x"}',
+    '{"type":"hello","version":"5.8.2"}'
+]
+
+for (const invalid_message of invalid_messages) {
+    console.log(invalid_message);
+    test_mal_messages(invalid_message);
+}
