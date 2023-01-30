a9e28207ae0a6a299b302ad02f2b500909e76bc8
ende <endeshen99@gmail.com>
2023-01-30 14:55:33 -0800

    block validation 5.

--- src/block.ts
+++ src/block.ts
@@ -1,6 +1,10 @@
 import { ObjectId, ObjectStorage } from "./store";
 import { AnnotatedError, BlockObjectType, TransactionInputObjectType, TransactionObjectType, TransactionOutputObjectType, OutpointObjectType, SpendingTransactionObject } from "./message";
 import { UTXOManager } from "./UTXOmanager";
+import { network } from './network'
+import delay from 'delay';
+
+const TIMEOUT_DELAY = 10000
 
 export class Block {
     objectid: ObjectId
@@ -52,6 +56,38 @@ export class Block {
         if (this.objectid > this.T) {
             throw new AnnotatedError('INVALID_BLOCK_POW', `Block ID isn't below target.`)
         }
+        const transactions_present = await Promise.all(
+            this.transactions.map(async (transaction) => {
+                const present = await ObjectStorage.exists(transaction)
+                if (!present) {
+                    network.broadcast({
+                        type: 'getobject',
+                        objectid: transaction
+                    })
+                }
+                return present
+            })
+        )
+        var missing_tx: string[] = [];
+        transactions_present.forEach((has_tx_id, tx_idx) => {
+            if (!has_tx_id) {
+                missing_tx.push(this.transactions[tx_idx])
+            }
+        })
+        if (missing_tx.length > 0) {
+            await delay(TIMEOUT_DELAY);
+        }
+        const transactoins_present_final = await Promise.all(
+            missing_tx.map(async (tx) => {
+                const present = await ObjectStorage.exists(tx)
+                return present
+            })
+        )
+        transactoins_present_final.forEach((has_tx, tx_idx) => {
+            if (!has_tx) {
+                throw new AnnotatedError('UNFINDABLE_OBJECT', `Cannot find tx ${missing_tx[tx_idx]}.`)
+            }
+        })
 
         // validate UTXO
         UTXOManager.extendUTXO(this.objectid)
