e263b299c1ff46b4c0e3377771ffe542cf707009
ende <endeshen99@gmail.com>
2023-01-30 18:09:01 -0800

    Block Validation 7 & 8

--- src/block.ts
+++ src/block.ts
@@ -17,6 +17,29 @@ export class Block {
     miner: string = ""
     note: string = ""
 
+    // checking two things here: Block Validation 7. & 8.
+    async resolveCoinbase(transactions: Transaction[]) {
+        var coinbaseTx_hash: string | null = null;
+        transactions.forEach((tx, tx_idx) => {
+            if (tx.height != null) { // this is a coinbase tx
+                if (tx_idx != 0) { // coinbase tx can only have tx_idx 0, i.e. be the first tx
+                    throw new AnnotatedError('INVALID_BLOCK_COINBASE', `Coinbase tx appeared @ ${tx_idx}`)
+                } else { // valid coinbase tx
+                    coinbaseTx_hash = tx.txid
+                }
+            } else { // verify Block Validation 8
+                if (coinbaseTx_hash != null) {
+                    var spend_tx_hashes = tx.inputs.map((input, input_idx)=>{
+                        return input.outpoint.txid
+                    })
+                    if (spend_tx_hashes.includes(coinbaseTx_hash)) {
+                        throw new AnnotatedError('INVALID_TX_OUTPOINT', 'Detect spending Coinbase Tx @ ${tx_idx}')
+                    }
+                }
+            }
+        })
+    }
+
     async resolve(transactions: ObjectId[]) {
         const transactions_present = await Promise.all(
             transactions.map(async (transaction) => {
@@ -94,14 +117,18 @@ export class Block {
             throw new AnnotatedError('INVALID_BLOCK_POW', `Block ID isn't below target.`)
         }
         await this.resolve(this.transactions)
+        var transactions_arr: Transaction[] = [];
         for (const tx of this.transactions){
             try {
-                await Transaction.fromNetworkObject(await ObjectStorage.get(tx)).validate()
+                const curr_tx = Transaction.fromNetworkObject(await ObjectStorage.get(tx))
+                transactions_arr.push(curr_tx)
+                await curr_tx.validate()
             }
             catch (e: any) {
                 throw new AnnotatedError('UNFINDABLE_OBJECT', `Transaction invalid: id-${tx}.`)
             }
         }
+        await this.resolveCoinbase(transactions_arr)
 
         // validate UTXO
         UTXOManager.extendUTXO(this.objectid)
