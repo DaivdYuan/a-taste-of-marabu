772ea35a450fad6e0349df5446d622ce4ea332b3
ende <endeshen99@gmail.com>
2023-02-22 11:43:40 -0800

    manually put genesis block into object storage

--- src/object.ts
+++ src/object.ts
@@ -97,6 +97,20 @@ class ObjectManager {
 
     return object
   }
+  constructor() {
+    const TARGET = '00000000abc00000000000000000000000000000000000000000000000000000'
+    const GENESIS: BlockObjectType = {
+      T: TARGET,
+      created: 1671062400,
+      miner: 'Marabu',
+      nonce: '000000000000000000000000000000000000000000000000000000021bea03ed',
+      note: 'The New York Times 2022-12-13: Scientists Achieve Nuclear Fusion Breakthrough With Blast of 192 Lasers',
+      previd: null,
+      txids: [],
+      type: 'block'
+    }
+    this.put(GENESIS)
+  }
 }
 
 export const objectManager = new ObjectManager()
